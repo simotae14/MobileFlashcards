9d63cf8a5ec9e5c9d1edfdaaaacf30621a86a803
Simone Taeggi <simone.taeggi@leovegas.com>
2018-09-24 11:18:56 +0200

    Created Actions

---
+++ actions/index.js
@@ -0,0 +1,24 @@
+export const GET_DECKS = 'GET_DECKS';
+export const ADD_QUESTION = 'ADD_QUESTION';
+export const ADD_DECK = 'ADD_DECK';
+
+export function getDecks(decks) {
+    return {
+        type: GET_DECKS,
+        decks
+    };
+}
+
+export function addQuestion(question) {
+    return {
+        type: ADD_QUESTION,
+        question
+    };
+}
+
+export function addDeck(deck) {
+    return {
+        type: ADD_DECK,
+        deck
+    };
+}
