9c5cabd1fae959d591742551f476fd9c0edfc37b
Simone Taeggi <simone.taeggi@leovegas.com>
2018-09-24 18:15:14 +0200

    Created the Deck List with random colors

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import { retrieveDecks } from '../actions';
 import { getDecks } from '../utils/decksStorageApi';
 import { connect } from 'react-redux';
+import randomColor from 'randomcolor';
 
 class DeckList extends React.Component {
     componentDidMount() {
@@ -12,11 +13,38 @@ class DeckList extends React.Component {
             .then((results) => dispatch(retrieveDecks(results)));
     }
     render() {
-        console.log('decks', this.props.decks);
+        const { decks } = this.props;
         return (
-            <View>
-                <Text>Decks</Text>
-                <Text>{JSON.stringify(this.props.decks)}</Text>
+            <View style={styles.container}>
+                {
+                    decks ? (
+                        <ScrollView>
+                            {
+                                Object.values(decks).map((deck) => {
+                                    return (
+                                        <View
+                                            key={deck.title}
+                                            style={[styles.deckItem, { backgroundColor: randomColor({ luminosity: 'dark' }) }]}
+                                        >
+                                            <Text
+                                                style={styles.deckText}
+                                            >
+                                                { deck.title }
+                                            </Text>
+                                            <Text
+                                                style={styles.deckNumberCards}
+                                            >
+                                                { deck.questions.length } cards
+                                            </Text>
+                                        </View>
+                                    );
+                                })
+                            }
+                        </ScrollView>
+                    ) : (
+                        <Text>No Decks available</Text>
+                    )
+                }
             </View>
         );
     }
@@ -29,3 +57,37 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(DeckList);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#ffe',
+        paddingTop: 50
+    },
+    deckItem: {
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        minHeight: 150,
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginBottom: 10,
+    },
+    deckText: {
+        color: 'white',
+        fontSize: 50
+    },
+    deckNumberCards: {
+        color: '#ddd',
+        fontSize: 30
+    }
+});
